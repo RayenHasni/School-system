fff0febc9a964b5c8f5340967568e76a1ec415a1
unknown <rayen.hasny23@gmail.com>
2023-12-17 10:26:30 +0100

    New Navbar & Footer

--- client/src/components/modules/Footer.js
+++ client/src/components/modules/Footer.js
@@ -1,16 +1,47 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import "./Footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faTwitter, faSnapchat} from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
   return (
-    <footer className="mt-auto py-3 bg-light">
-      <Container>
-        <Row>
-          <Col className="text-center">
-            <span>&copy; 2023 School System. All rights reserved.</span>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+    <div className="footer-basic">
+  <footer>
+    <div className="social">
+      <a href="#">
+      <FontAwesomeIcon icon={faInstagram} />
+        <i className="icon ion-social-instagram" />
+      </a>
+      <a href="#">
+      <FontAwesomeIcon icon={faFacebook} />
+      </a>
+      <a href="#">
+      <FontAwesomeIcon icon={faSnapchat} />
+      </a>
+      <a href="#">
+      <FontAwesomeIcon icon={faTwitter} />
+      </a>
+    </div>
+    <ul className="list-inline">
+      <li className="list-inline-item">
+        <a href="#">Home</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">Services</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">About</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">Terms</a>
+      </li>
+      <li className="list-inline-item">
+        <a href="#">Privacy Policy</a>
+      </li>
+    </ul>
+    <p className="copyright">School © 2023</p>
+  </footer>
+</div>
+
   );
 }
 
