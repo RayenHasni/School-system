726978162bf74321d58b0757f56ca65c825c4499
unknown <rayen.hasny23@gmail.com>
2023-12-17 18:16:27 +0100

    Dashboard work

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/login.js";
 import Register from "./pages/register.js";
 import Home from "./pages/home";
 import ProfilePage from "./pages/profile.js";
+import Dashboard from "./components/dashboard/dashboard.js"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/profile" element={<ProfilePage/>}/>
+          <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
       </Router>
     </div>
