dddbacf907c720c423f0badcde41fa17fba14606
dhiamlayah <113603828+dhiamlayah@users.noreply.github.com>
2023-12-17 18:34:25 +0000

    fix problems

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className="footer-basic">
     <footer>
+    <hr className="mx-5 d-flex justify-content-center "/>
     <p className="copyright text-dark fs-5">School © 2023</p>
     </footer>
     </div>
